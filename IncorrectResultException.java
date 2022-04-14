e595840574d305535c1025be4dd29f4c0e6eb9cd
sj185226 <sj185226@ncr.com>
2022-04-14 16:38:22 +0530

    added size mismatch param

--- IncorrectResultException.java
+++ IncorrectResultException.java
@@ -6,15 +6,20 @@ public class IncorrectResultException extends Exception {
     private List<Integer> values;
     private List<List<Integer>> expectedResult;
     private List<List<Integer>> actualResult;
-    IncorrectResultException(int batchSize, List<Integer> values, List<List<Integer>> expectedResult, List<List<Integer>> actualResult) {
+    private String pos;
+    IncorrectResultException(int batchSize, List<Integer> values, List<List<Integer>> expectedResult, List<List<Integer>> actualResult, String pos) {
         super();
         this.actualResult = actualResult;
         this.batchSize = batchSize;
         this.expectedResult = expectedResult;
         this.values = values;
+        this.pos = pos;
     }
 
     public void printInfo() {
+        if(pos != null) {
+            System.err.println("SizeMismatch for two results at " + pos);
+        }
         System.err.println("Expected didn't matched with actual result for:-");
         System.err.println("batch size: " + batchSize);
         System.err.println("values: " + values);
